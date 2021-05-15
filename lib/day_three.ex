155768b8ec9f270ad42eb35176c6885e035472c8
Grant Horner <h.grant.horner@gmail.com>
2021-05-14 23:11:20 -0400

    more set up for 3

--- lib/day_three.ex
+++ lib/day_three.ex
@@ -1,11 +1,20 @@
 defmodule DayThree do
   def part_one(path) do
     {:ok, content} = File.read(path)
+    toboggan = {0, 0}
     :ok
   end
 
   def part_two(path) do
     {:ok, content} = File.read(path)
     :ok
+  end
+
+  defp move_toboggan({x, y}, delta_x, delta_y) do
+    {x + delta_x, y + delta_y}
+  end
+
+  defp wrap_toboggan({x, y}, max_x, max_y) do
+
   end
 end
